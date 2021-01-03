b192c50e6db4e3a99f9aa2ede7176980d09a08a3
Bill Katz <billkatz@gmail.com>
2021-01-03 17:02:59 -0500

    handle CORS preflight-request

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import Depends, FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from dependencies import get_user
 from services import annotations, atlas, datasets, image_query, image_transfer, kv, savedsearches
 
 app = FastAPI()
 
+# Wire in the API endpoints
 # require user authorization for any of the actual data API calls
 app.include_router(annotations.router, dependencies=[Depends(get_user)])
 app.include_router(atlas.router, dependencies=[Depends(get_user)])
@@ -15,6 +17,15 @@ app.include_router(image_transfer.router, dependencies=[Depends(get_user)])
 app.include_router(kv.router, dependencies=[Depends(get_user)])
 app.include_router(savedsearches.router, dependencies=[Depends(get_user)])
 #app.include_router(users.router)
+
+# Handle CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=False, # Can't be True (cookies supported for CORS) if origins=*
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # allow unauthenticated to access root documentation
 @app.get("/", response_class=HTMLResponse)
@@ -29,3 +40,4 @@ async def root():
     </ul>
     </html>
     """
+

f335822788712001001a550aae8114f541723551
Bill Katz <billkatz@gmail.com>
2021-04-23 19:47:34 -0400

    fix json-annotations query and allow arbitrary bodyid lists

--- services/annotations_global.py
+++ services/annotations_global.py
@@ -32,44 +32,17 @@ def check_reserved_fields(data: List[dict]):
             if field.startswith("_"):
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"cannot have fields starting with underscore since those names are reserved")
 
-def reconcile_single_id(results, version, changes):
-    """for given results (list of snapshsots with decreasing timestamp), return 
-       best object that matches the given version
-    
-    Args:
-        results (Query results): assumed to be ordered by descending timestamp
-        version (str): version desired, assumed that later versions are 
-            lexicographically larger and None or empty string return most recent
-            version.
-        changes (bool): if true, return all changes to the annotation.
-    """
-    if changes:
-        output = []
-        for doc in results:
-            data = doc.to_dict()
-            if version is None or version == "" or data["_version"] <= version:
-                remove_reserved_fields(data)
-                output.append(data)
-        return output
-
-    best_version = ""
-    best_data = {}
-    versionless = (version is None or version == "")
+def get_changes(results, version: str):
+    output = []
     for doc in results:
         data = doc.to_dict()
-        if versionless:
-            if best_version < data["_version"]:
-                best_version = data["_version"]
-                best_data = data
-        elif data["_version"] <= version and data["_version"] > best_version:
-            best_version = data["_version"]
-            best_data = data
-    remove_reserved_fields(best_data)
-    return best_data
+        if version is None or version == "" or data["_version"] <= version:
+            remove_reserved_fields(data)
+            output.append(data)
+    return output
 
-
-def reconcile_query(results, id_field: str, version: str, changes: bool):
-    """Returns list of annotations for given version.
+def reconcile_query(results, id_field: str, version: str):
+    """Returns dict of id key with value of annotations for given version.
     
     Args:
         results (Query results): list of JSON objects that meet query.
@@ -79,16 +52,6 @@ def reconcile_query(results, id_field: str, version: str, changes: bool):
             version.
         changes (bool): if true, return all changes to the annotations.
     """
-    # group docs by id field
-    if changes:
-        output = []
-        for doc in results:
-            data = doc.to_dict()
-            if version is None or version == "" or data["_version"] <= version:
-                remove_reserved_fields(data)
-                output.append(data)
-        return output
-
     # prune by version
     pruned_results = []
     for doc in results:
@@ -121,6 +84,24 @@ def reconcile_query(results, id_field: str, version: str, changes: bool):
         remove_reserved_fields(data)
     return list(best.values())
 
+def run_query_with_ids(query, ids: List[int], id_field: str, version: str, changes: bool):
+    """ Run query (without id_field selector) across an arbitrary number of ids. """
+    output = []
+    for start in range(0, len(ids), 10):
+        remain = min(len(ids) - start, 10)
+        if remain == 1:
+            value = ids[start]
+            op = '=='
+        else:
+            value = ids[start:start+remain]
+            op = 'in'
+        results = query.where(id_field, op, value).get()
+        if changes:
+            output.append(get_changes(results, version))
+        else:
+            output.extend(reconcile_query(results, id_field, version))
+
+    return output
 
 def write_annotation(version, collection, data, user: User):
     data["_version"] = version
@@ -157,23 +138,10 @@ def get_annotations(dataset: str, annotation_type: str, id: str, version: str = 
         ids = [int(id_str) for id_str in id_strs]
     else:
         ids = [int(id)]
-    print(ids)
     
     try:
-        collection = firestore.get_collection([CLIO_ANNOTATIONS_GLOBAL, annotation_type, dataset])
-        if len(ids) == 1:
-            results = collection.where(id_field, u'==', ids[0]).get()
-            return reconcile_single_id(results, version, changes)
-        else:
-            data = []
-            for i in ids:
-                results = collection.where(id_field, u'==', i).get()
-                reconciled = reconcile_single_id(results, version, changes)
-                if changes:
-                    data.extend(reconciled)
-                else:
-                    data.append(reconciled)
-            return data
+        q = firestore.get_collection([CLIO_ANNOTATIONS_GLOBAL, annotation_type, dataset])
+        return run_query_with_ids(q, ids, id_field, version, changes)
 
     except Exception as e:
         print(e)
@@ -195,7 +163,7 @@ def get_annotations(dataset: str, annotation_type: str, query: dict, version: st
 
         changes (bool): If True, returns list of changes to this annotation across all versions.
 
-        id_field (str): The id field name (default: "bodyid")
+        id_field (str): The id field name (default: "bodyid") that should be integers.
 
     Returns:
 
@@ -207,7 +175,17 @@ def get_annotations(dataset: str, annotation_type: str, query: dict, version: st
     try:
         collection = firestore.get_collection([CLIO_ANNOTATIONS_GLOBAL, annotation_type, dataset])
         q = collection
+        conditionals = 0
+        ids = []
         for key in query:
+            if key == id_field:
+                if isinstance(query[key], int):
+                    ids = [query[key]]
+                elif isinstance(query[key], list):
+                    ids = query[key]
+                else:
+                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"id field must be int or list of ints, got: {query[key]}")
+                continue
             if isinstance(query[key], list):
                 if len(query[key]) > 10:
                     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"currently no more than 10 values can be queried at a time")
@@ -215,11 +193,19 @@ def get_annotations(dataset: str, annotation_type: str, query: dict, version: st
             else:
                 op = "=="
             q = q.where(key, op, query[key])
-        results = q.get()
-        if len(results) == 0:
-            return []
-        output = reconcile_query(results, id_field, version, changes)
-        return output
+            conditionals += 1
+
+        if len(ids) == 0:
+            if conditionals == 0:
+                return []
+            results = q.get()
+            if changes:
+                return get_changes(results, version)
+            else:
+                return reconcile_query(results, id_field, version)
+        else: # add ability to have query for more than 10 body ids.
+            return run_query_with_ids(q, ids, id_field, version, changes)
+
 
     except Exception as e:
         print(e)
